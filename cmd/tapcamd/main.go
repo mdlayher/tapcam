02f4187ed8aaca6d0fc88ce241730d240d68f14f
Matt Layher <mdlayher@gmail.com>
2016-01-05 17:22:52 -0500

    [WIP] tapcamd: image resizing

--- cmd/tapcamd/main.go
+++ cmd/tapcamd/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"flag"
+	"image"
+	"image/jpeg"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/nfnt/resize"
 )
 
 var (
@@ -20,9 +27,60 @@ func main() {
 	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(*dir)))
+	mux.Handle("/", &Handler{
+		dir: *dir,
+	})
 
 	if err := http.ListenAndServe(*host, mux); err != nil {
 		log.Fatal(err)
 	}
+}
+
+type Handler struct {
+	dir string
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
+	width := q.Get("width")
+	if width == "" {
+		http.FileServer(http.Dir(h.dir)).ServeHTTP(w, r)
+		return
+	}
+
+	iwidth, err := strconv.Atoi(width)
+	if err != nil {
+		http.Error(w, "bad width value", http.StatusBadRequest)
+		return
+	}
+
+	f, err := os.Open(filepath.Join(
+		h.dir,
+		filepath.Base(r.URL.Path),
+	))
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+	defer func() { _ = f.Close() }()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	img = resize.Resize(uint(iwidth), 0, img, resize.NearestNeighbor)
+	var buf bytes.Buffer
+	if err := jpeg.Encode(&buf, img, nil); err != nil {
+		log.Println(err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "image/jpeg")
+
+	_, _ = buf.WriteTo(w)
 }
